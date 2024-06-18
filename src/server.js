fe0c54d5a889724b834c16bab36ab751d208f06b
OlenaYurchuk <145892734+OlenaYurchuk@users.noreply.github.com>
2024-06-18 13:08:50 +0300

    added status field

--- src/server.js
+++ src/server.js
@@ -27,25 +27,21 @@ export const setupServer = () => {
         });
     });
 
-    app.get('/contacts', async (req, res) => {
+    app.get('/contacts', async (req, res, next) => {
         try {
             const contacts = await getAllContacts();
 
-            return res.status(200).json({
-                status: 200,
-                data: contacts,
-                message: 'Successfully found contacts',
+                res.status(200).json({
+                    status: 200,
+                    message: 'Successfully found contacts',
+                    data: contacts,
             });
         } catch (error) {
-            return res.status(500).json({
-                status: 500,
-                message: 'Something went wrong',
-                error: error.message
-            });
+            next(error);
         }
     });
 
-    app.get('/contacts/:contactId', async (req, res) => {
+    app.get('/contacts/:contactId', async (req, res, next) => {
         const { contactId } = req.params;
 
         if (!mongoose.Types.ObjectId.isValid(contactId)) {
@@ -63,29 +59,25 @@ export const setupServer = () => {
                 });
             }
 
-            return res.status(200).json({
+            res.status(200).json({
                 status: 200,
                 data: contact,
                 message: `Successfully found contact with id ${contactId}!`
             });
         } catch (error) {
-            return res.status(500).json({
-                status: 500,
-                message: 'Something went wrong',
-                error: error.message,
-            });
+            next(error);
         }
     });
 
     app.use("*", (req, res) => {
-        return res.status(404).json({
+        res.status(404).json({
             status: 404,
             message: "Not Found"
         });
     });
 
     app.use((err, req, res) => {
-        return res.status(500).json({
+        res.status(500).json({
             status: 500,
             message: "Something went wrong",
             error: err.message,
